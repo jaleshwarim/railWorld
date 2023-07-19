c338d24ebffc4ef73d50d0c19b60fabc5422c5d1
jaleshwarim <jaleshwarimasram@gmail.com>
2023-07-19 13:10:58 +0530

    Sts file upload

---
+++ Sts-Project/Interface/src/com/test/Ractangle.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Ractangle implements Shap{
+
+	@Override
+	public void shap() {
+		// TODO Auto-generated method stub
+		System.out.println("This shap is Ractangle");
+		
+	}
+
+}
---
+++ Sts-Project/Interface/src/com/test/Squar.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class Squar implements Shap{
+	
+	public  void shap() {
+		
+		System.out.println("This shap ios Squaer");
+		
+	}
+
+}
