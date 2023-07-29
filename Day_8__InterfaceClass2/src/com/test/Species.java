2224ed32227bfdf11270dc0cc9364f7bfc252940
jaleshwarim <jaleshwarimasram@gmail.com>
2023-07-29 14:56:54 +0530

    Assignment

--- Day_8__InterfaceClass2/src/com/test/Species.java
+++
@@ -1,9 +0,0 @@
-package com.test;
-
-public  interface  Species {
-
-	
-	public static void info() {};
-	abstract void typeOfSpe();
-	abstract void lifeSpe();
-}
--- Day_8__InterfaceClass2/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module InterfaceClass2 {
-}
